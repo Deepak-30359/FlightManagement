4e78e2c36aaf0f032103a9237221bc5f08d8bfdb
Deepak-30359 <85355647+Deepak-30359@users.noreply.github.com>
2021-10-19 15:53:14 +0530

    FlightMan

---
+++ src/main/java/com/flight/exception/ScheduledFlightNotFoundException.java
@@ -0,0 +1,13 @@
+package com.flight.exception;
+
+public class ScheduledFlightNotFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ScheduledFlightNotFoundException(String str) {
+		super(str);
+	}
+
+}
